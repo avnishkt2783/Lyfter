5f2cc13480913e182b6f8e8363d80759efecada2
simransahiwal <sahiwalsimran@gmail.com>
2025-05-13 11:11:16 +0530

    passengerdriver logic and 2 pages

--- backend/routes/rideRoutes.js
+++ backend/routes/rideRoutes.js
@@ -1,12 +1,22 @@
 import express from "express";
-import { offerRideDetails, requestRideDetails, matchingRides } from "../controllers/rideController.js";
+import { offerRideDetails, requestRideDetails, matchingRides, getOfferedRides, getPendingRequests, updateRequestStatus, rideAction } from "../controllers/rideController.js";
 
 const router = express.Router();
 
 // POST /api/rides
 router.post("/offerRideDetails", offerRideDetails);
 router.post("/requestRideDetails",requestRideDetails);
-router.post("/matchingRides", matchingRides)
-// router.post("/createPassengerRideDriverRide", createPassengerRideDriverRide)
+router.post("/matchingRides", matchingRides);
+
+router.get("/yourofferedrides", getOfferedRides);  // Fetch all rides offered by a driver
+
+// GET /api/rides/pending-requests/:driverRideId
+router.get("/pendingrequests/:driverRideId", getPendingRequests);  // Get pending requests for a ride
+
+// POST /api/rides/update-request-status
+router.post("/updaterequeststatus", updateRequestStatus);  // Update request status (Accepted or Rejected)
+
+// POST /api/rides/ride-action
+router.post("/rideaction", rideAction);  // Action (Start, Cancel, Finish Ride)
 
 export default router;
