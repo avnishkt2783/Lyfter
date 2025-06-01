55435c3f03f8354876b037f982a369f43dce82ce
Avnish Kumar <108517108+avnishkt2783@users.noreply.github.com>
2025-06-01 23:39:31 +0530

    Update RedirectExternal.jsx

--- frontend/src/components/RedirectExternal.jsx
+++ frontend/src/components/RedirectExternal.jsx
@@ -3,7 +3,7 @@ import { useEffect } from "react";
 
 const RedirectExternal = () => {
   useEffect(() => {
-    window.location.replace("https://www.youtube.com");
+    window.location.replace("https://www.youtube.com/watch?v=XsfTCT3DsmA");
   }, []);
 
   return null; // Don't render anything
